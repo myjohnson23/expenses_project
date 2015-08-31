a37f5f11e63ae353a468c62d062923c63794121a
myjohnson23 <myjohnson@gmail.com>
2015-08-31 11:59:37 -0500

    Backbone models and collections for View added

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,47 @@
+var app = app || {};
+var active = active || {};
+
+//blueprints
+app.model = Backbone.Model.extend();
+app.collection = Backbone.Collection.extend({
+  model: app.model,
+  url: '/api/expense'
+});
+app.modelView = Backbone.View.extend({
+  initialize: function() {
+    this.template = _.template($('#expenses-template').html());
+    this.render();
+  },
+  render: function() {
+    var data = this.model.attributes;
+    this.$el.append(this.template(data));
+  }
+});
+app.collectionView = Backbone.View.extend({
+  initialize: function() {
+    var that = this;
+    this.collection.on('sync', function() {
+      that.render();
+    });
+    this.collection.fetch();
+    this.$el.html('');
+  },
+  render: function() {
+    var collection = this.collection.models;
+    for (var model in collection) {
+      new app.modelView({
+        el: $('#expenses-list'),
+        model: collection[model]
+      });
+    }
+  }
+});
+//end blueprints
+
+$(document).ready(function(event){
+  active.collection = new app.collection();
+  active.collectionView = new app.collectionView({
+    collection: active.collection,
+    el: $('#expenses-list')
+  });
+});
